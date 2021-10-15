bede1f2627ba845994474070cd30ee72f7b1c40d
hfpp2012 <hfpp2012@gmail.com>
2021-10-15 11:37:25 +0800

    Dark theme: feature section

--- components/Index/FeatureSection.js
+++ components/Index/FeatureSection.js
@@ -34,13 +34,13 @@ const features = [
 
 export default function FeatureSection() {
   return (
-    <div className="py-12 bg-white">
+    <div className="py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="lg:text-center">
           <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
             Transactions
           </h2>
-          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl dark:text-white">
             A better way to send money
           </p>
           <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
