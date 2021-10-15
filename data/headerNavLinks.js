26429a6c43e01d987c1da607916d503dec601f23
hfpp2012 <hfpp2012@gmail.com>
2021-10-15 16:44:44 +0800

    add export script

---
+++ data/headerNavLinks.js
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/about', title: 'About' },
+  { href: '/posts', title: 'Old Posts' },
+]
+
+export default headerNavLinks
