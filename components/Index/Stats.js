ddc86e812a76603f38a3025ca903435d74995d66
hfpp2012 <hfpp2012@gmail.com>
2021-10-15 11:52:42 +0800

    Dark theme: Stats component

--- components/Index/Stats.js
+++ components/Index/Stats.js
@@ -1,9 +1,9 @@
 export default function Stats() {
   return (
-    <div className="bg-gray-50 pt-12 sm:pt-16">
+    <div className="bg-gray-50 dark:bg-gray-900 pt-12 sm:pt-16">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="max-w-4xl mx-auto text-center">
-          <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
+          <h2 className="text-3xl dark:text-white font-extrabold text-gray-900 sm:text-4xl">
             Trusted by developers from over 80 planets
           </h2>
           <p className="mt-3 text-xl text-gray-500 sm:mt-4">
@@ -12,12 +12,12 @@ export default function Stats() {
           </p>
         </div>
       </div>
-      <div className="mt-10 pb-12 bg-white sm:pb-16">
+      <div className="mt-10 pb-12 dark:bg-gray-900 sm:pb-16">
         <div className="relative">
-          <div className="absolute inset-0 h-1/2 bg-gray-50" />
+          <div className="absolute inset-0 h-1/2 bg-gray-50 dark:bg-gray-900" />
           <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="max-w-4xl mx-auto">
-              <dl className="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-3">
+              <dl className="rounded-lg shadow-lg sm:grid sm:grid-cols-3">
                 <div className="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
                   <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
                     Pepperoni
