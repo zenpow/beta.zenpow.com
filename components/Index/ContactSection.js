a3b2acd260fec239f80647c89fb8ade3048ba82a
hfpp2012 <hfpp2012@gmail.com>
2021-10-15 12:05:17 +0800

    Dark theme: Contact sales

--- components/Index/ContactSection.js
+++ components/Index/ContactSection.js
@@ -10,10 +10,10 @@ export default function ContactSection() {
   const [agreed, setAgreed] = useState(false)
 
   return (
-    <div className="bg-white py-16 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-24">
+    <div className="bg-white dark:bg-gray-900 py-16 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-24">
       <div className="relative max-w-xl mx-auto">
         <svg
-          className="absolute left-full transform translate-x-1/2"
+          className="absolute left-full transform translate-x-1/2 dark:hidden"
           width={404}
           height={404}
           fill="none"
@@ -42,7 +42,7 @@ export default function ContactSection() {
           <rect width={404} height={404} fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
         </svg>
         <svg
-          className="absolute right-full bottom-0 transform -translate-x-1/2"
+          className="absolute right-full bottom-0 transform -translate-x-1/2 dark:hidden"
           width={404}
           height={404}
           fill="none"
@@ -71,7 +71,7 @@ export default function ContactSection() {
           <rect width={404} height={404} fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
         </svg>
         <div className="text-center">
-          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          <h2 className="text-3xl dark:text-white font-extrabold tracking-tight text-gray-900 sm:text-4xl">
             Contact sales
           </h2>
           <p className="mt-4 text-lg leading-6 text-gray-500">
@@ -86,7 +86,10 @@ export default function ContactSection() {
             className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
           >
             <div>
-              <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="first-name"
+                className="block text-sm font-medium text-gray-700 dark:text-white"
+              >
                 First name
               </label>
               <div className="mt-1">
@@ -95,12 +98,15 @@ export default function ContactSection() {
                   name="first-name"
                   id="first-name"
                   autoComplete="given-name"
-                  className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                  className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md dark:text-black"
                 />
               </div>
             </div>
             <div>
-              <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="last-name"
+                className="block text-sm font-medium text-gray-700 dark:text-white"
+              >
                 Last name
               </label>
               <div className="mt-1">
@@ -109,12 +115,15 @@ export default function ContactSection() {
                   name="last-name"
                   id="last-name"
                   autoComplete="family-name"
-                  className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                  className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md dark:text-black"
                 />
               </div>
             </div>
             <div className="sm:col-span-2">
-              <label htmlFor="company" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="company"
+                className="block text-sm font-medium text-gray-700 dark:text-white"
+              >
                 Company
               </label>
               <div className="mt-1">
@@ -123,12 +132,15 @@ export default function ContactSection() {
                   name="company"
                   id="company"
                   autoComplete="organization"
-                  className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                  className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md dark:text-black"
                 />
               </div>
             </div>
             <div className="sm:col-span-2">
-              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700 dark:text-white"
+              >
                 Email
               </label>
               <div className="mt-1">
@@ -137,12 +149,15 @@ export default function ContactSection() {
                   name="email"
                   type="email"
                   autoComplete="email"
-                  className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                  className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md dark:text-black"
                 />
               </div>
             </div>
             <div className="sm:col-span-2">
-              <label htmlFor="phone-number" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="phone-number"
+                className="block text-sm font-medium text-gray-700 dark:text-white"
+              >
                 Phone Number
               </label>
               <div className="mt-1 relative rounded-md shadow-sm">
@@ -153,7 +168,7 @@ export default function ContactSection() {
                   <select
                     id="country"
                     name="country"
-                    className="h-full py-0 pl-4 pr-8 border-transparent bg-transparent text-gray-500 focus:ring-indigo-500 focus:border-indigo-500 rounded-md"
+                    className="h-full py-0 pl-4 pr-8 border-transparent bg-transparent text-gray-500 focus:ring-indigo-500 focus:border-indigo-500 rounded-md dark:text-black"
                   >
                     <option>US</option>
                     <option>CA</option>
@@ -165,13 +180,16 @@ export default function ContactSection() {
                   name="phone-number"
                   id="phone-number"
                   autoComplete="tel"
-                  className="py-3 px-4 block w-full pl-20 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                  className="py-3 px-4 block w-full pl-20 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md dark:text-black"
                   placeholder="+1 (555) 987-6543"
                 />
               </div>
             </div>
             <div className="sm:col-span-2">
-              <label htmlFor="message" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="message"
+                className="block text-sm font-medium text-gray-700 dark:text-white"
+              >
                 Message
               </label>
               <div className="mt-1">
@@ -179,7 +197,7 @@ export default function ContactSection() {
                   id="message"
                   name="message"
                   rows={4}
-                  className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border border-gray-300 rounded-md"
+                  className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border border-gray-300 rounded-md dark:text-black"
                   defaultValue={''}
                 />
               </div>
@@ -209,11 +227,15 @@ export default function ContactSection() {
                   <p className="text-base text-gray-500">
                     By selecting this, you agree to the{' '}
                     <Link href="/">
-                      <a className="font-medium text-gray-700 underline">Privacy Policy</a>
+                      <a className="font-medium text-gray-700 underline dark:text-white">
+                        Privacy Policy
+                      </a>
                     </Link>{' '}
                     and{' '}
                     <Link href="/">
-                      <a className="font-medium text-gray-700 underline">Cookie Policy</a>
+                      <a className="font-medium text-gray-700 underline dark:text-white">
+                        Cookie Policy
+                      </a>
                     </Link>
                     .
                   </p>
