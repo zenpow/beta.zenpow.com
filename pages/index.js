d59124bdaa839bc2c34bbc33590d65c1eb53a570
hfpp2012 <hfpp2012@gmail.com>
2021-10-15 12:12:07 +0800

    Add seo for home page

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,13 @@ import Stats from '../components/Index/Stats'
 import Testimonials from '../components/Index/Testimonials'
 import LogoClouds from '../components/Index/LogoClouds'
 import ContactSection from '../components/Index/ContactSection'
+import { PageSEO } from '@/components/SEO'
+import siteMetadata from '@/data/siteMetadata'
 
 export default function Index() {
   return (
     <>
+      <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <HeroSection />
       <FeatureSection />
       <HeaderSection />
