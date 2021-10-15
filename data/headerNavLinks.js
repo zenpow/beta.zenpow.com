02e947590f7ffcdd6907f2355793bf75ffcb4c62
hfpp2012 <hfpp2012@gmail.com>
2021-10-15 11:24:37 +0800

    Add narbar link for old blogs page

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: '/posts', title: 'Old Posts' },
 ]
 
 export default headerNavLinks
