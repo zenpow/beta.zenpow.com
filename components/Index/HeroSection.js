97d88e63656bfeec55782f831f05ef2660077e49
hfpp2012 <hfpp2012@gmail.com>
2021-10-15 11:36:41 +0800

    Dark theme: hero section

--- components/Index/HeroSection.js
+++ components/Index/HeroSection.js
@@ -2,12 +2,12 @@ import Link from 'next/link'
 
 export default function HeroSection() {
   return (
-    <div className="relative bg-gray-50 overflow-hidden">
+    <div className="relative overflow-hidden">
       <div className="relative pt-6 pb-16 sm:pb-24">
         <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
           <div className="text-center">
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-              <span className="block xl:inline">Data to enrich your</span>{' '}
+              <span className="block xl:inline dark:text-white">Data to enrich your</span>{' '}
               <span className="block text-indigo-600 xl:inline">online business</span>
             </h1>
             <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
