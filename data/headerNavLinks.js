996f2843d9e6d3e58390f94bc11997c2f181f95a
hfpp2012 <hfpp2012@aliyun.com>
2021-10-18 03:06:15 +0000

    Deploying to gh-pages from @ zenpow/beta.zenpow.com@7f3bcb9fd0ee73b4705b37d0fee9be0426c7ba99 🚀

--- data/headerNavLinks.js
+++
@@ -1,9 +0,0 @@
-const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
-  { href: '/posts', title: 'Old Posts' },
-]
-
-export default headerNavLinks
