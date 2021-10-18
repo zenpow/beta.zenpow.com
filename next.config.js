09b1ff5ac338e9d9e27acab1e4ed78ab167a8952
Hopper Gee <hopper.gee@hey.com>
2021-10-18 15:17:38 +0800

    Remove basepath

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 module.exports = withBundleAnalyzer({
-  basePath: '/beta.zenpow.com',
   images: {
     domains: ['tailwindui.com'],
     loader: 'custom',
