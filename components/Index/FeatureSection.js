cec8e83d721b3befb06d786a77143555d35af5fe
Zac Zheng <zac@zenpow.com>
2021-10-27 11:09:50 +0100

    updated with some content

--- components/Index/FeatureSection.js
+++ components/Index/FeatureSection.js
@@ -38,10 +38,10 @@ export default function FeatureSection() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="lg:text-center">
           <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
-            Transactions
+            Why Us
           </h2>
           <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl dark:text-white">
-            A better way to send money
+            Our web apps having been running for 13+ years
           </p>
           <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
             Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum
